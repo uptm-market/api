f241ebc37d4c025c32e712b11c845bf7009c3e9c
mateus-henrique-silva <henrique.s@gsuite.iff.edu.br>
2024-02-08 15:38:17 -0300

    feat:subir script sql

--- postgres/create.sql
+++ postgres/create.sql
@@ -1,7 +1,6 @@
-
-create Table users(
+CREATE TABLE users (
     id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     email_verification_key VARCHAR(255),
     email_verification_time TIMESTAMP,
     password VARCHAR(255) NOT NULL,
@@ -14,12 +13,24 @@ create Table users(
     street VARCHAR(255),
     street_number VARCHAR(20),
     complement VARCHAR(255),
-    birthdate VARCHAR(20),
-    gender VARCHAR(10),
+    birthdate DATE,
+    gender ENUM('Male', 'Female', 'Other'),
     cpf VARCHAR(14),
     source TINYINT,
-    record_date TIMESTAMP,
+    record_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     level INTEGER
 );
 
+CREATE TABLE user_sub_accounts (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    key_api VARCHAR(255) UNIQUE
+);
 
+CREATE TABLE related_sub_accounts_in_master_user (
+    sub_account_id INT,
+    user_id INT,
+    PRIMARY KEY (sub_account_id, user_id),
+    FOREIGN KEY (sub_account_id) REFERENCES user_sub_accounts(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
