b83cb979297fed6abb06d2642e4d5f786e63be62
mateus-henrique-silva <henrique.s@gsuite.iff.edu.br>
2024-05-23 13:14:37 -0300

    .

--- db/user.go
+++ db/user.go
@@ -17,7 +17,7 @@ func Create(ctx context.Context, user entity.UserCreations) error {
 		name,
 		level
 	) VALUES (
-		$1, 'verification', NOW(), $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15
+		$1,  $2, $3, $4
 	)
 	`
 
