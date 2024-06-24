8ddc120f5d7610cc8d27e67f8e5f6958fbcae219
mateus-henrique-silva <henrique.s@gsuite.iff.edu.br>
2024-06-24 10:08:05 -0300

    subindo ajuste

--- postgres/create.sql
+++ postgres/create.sql
@@ -41,7 +41,7 @@ CREATE TABLE related_sub_accounts_in_master_user (
 CREATE TABLE facebook_campaign_ad_account (
     id INT PRIMARY KEY ,
     app_secret VARCHAR(255) ,
-    business_id ,
+    business_id VARCHAR(255),
     user_id INT NOT NULL, 
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
